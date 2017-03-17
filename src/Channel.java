b1118981d65d0842f8a43bb90b7c30c0d7d8d0f6
svetlana <sgaloshina@mail.ru>
2017-03-17 09:49:59 +0300

    Channel

--- src/Channel.java
+++ src/Channel.java
@@ -4,11 +4,38 @@ import java.util.LinkedList;
  * Created by svetlana on 17.03.17.
  */
 public class Channel {
+    private static final Object lock = new Object();    // монитор
     private int maxCount = 0;
     private final LinkedList<Runnable>  linkedList = new LinkedList<Runnable>();
 
-    public Channel(int maxCount) { this.maxCount = maxCount; }
-    void put(Runnable x) { linkedList.addLast(x); }
-    Runnable take() { return linkedList.removeFirst(); }
+    public Channel(int maxCount) {
+        this.maxCount = maxCount;
+    }
+    void put(Runnable x) {
+        synchronized (lock) {
+            while(linkedList.size() == maxCount) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            linkedList.addLast(x);
+            lock.notifyAll();
+        }
+    }
+    Runnable take() {
+        synchronized (lock) {
+            while(linkedList.isEmpty()) {
+                try {
+                    lock.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            lock.notifyAll();
+            return linkedList.removeFirst();
+        }
+    }
 
 }
